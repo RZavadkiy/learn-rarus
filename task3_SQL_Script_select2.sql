109969e8fbaf5e444747c3009422e59b3fb161d4
RZavadkiy <100112530+RZavadkiy@users.noreply.github.com>
2022-02-21 09:13:45 +0300

    erorr

--- task3_SQL_Script_select2.sql
+++
@@ -1,6 +0,0 @@
-select avtor.firstname, avtor.lastname
-from avtor
-where avtor.idavtor=(select book_has_avtor.Avtor_idAvtor
-from book_has_avtor
-group by Avtor_idAvtor
-ORDER BY count(book_has_avtor.Avtor_idAvtor) DESC LIMIT 1)
